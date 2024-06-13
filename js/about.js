eb98e45060f4cf5f6b10f055522404a7470b4654
Asad <uhope1645@gmail.com>
2024-06-13 21:49:11 +0500

    Adding more feature

---
+++ js/about.js
@@ -0,0 +1,19 @@
+
+
+let burgerBtn = document.querySelector('.burger')
+let partTwo = document.querySelector('.partTwo');
+
+
+
+let flag = 0
+
+burgerBtn.addEventListener('click', () => {
+    if (flag == 0) {
+      partTwo.style.transform = "translateX(0)"
+      flag = 1
+    } else {
+      partTwo.style.transform = "translateX(100%)"
+      flag = 0
+    }
+})
+
