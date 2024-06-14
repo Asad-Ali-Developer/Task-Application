a8b7d1e98849e8354968974705abdf808e7f80dc
Asad <uhope1645@gmail.com>
2024-06-14 16:15:18 +0500

    Done! Did work on responsiveness

--- js/scripts.js
+++ js/scripts.js
@@ -176,15 +176,19 @@ let partTwo = document.querySelector('.partTwo')
 
 let flag = 0
 
+
 burgerBtn.addEventListener('click', () => {
     if (flag == 0) {
-        partTwo.style.transform = 'translateX(0)'
-        flag = 1
+      partTwo.style.transform = "translateX(0)";
+      partTwo.style.display = "flex";
+      flag = 1
     } else {
-        partTwo.style.transform = 'translateX(100%)'
-        flag = 0
+      partTwo.style.transform = "translateX(100%)";
+      partTwo.style.display = "none";
+      flag = 0
     }
 })
+
 
 
 let searchBar = document.querySelector('#searchBarIn')
