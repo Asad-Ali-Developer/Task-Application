9cd2bea67adfcf44d52eb984d804a39f91a74f19
Asad <uhope1645@gmail.com>
2024-06-14 11:10:44 +0500

    Almost Done / Responsiveness - Index.html

--- js/index.js
+++ js/index.js
@@ -11,10 +11,12 @@ let flag = 0
 
 burgerBtn.addEventListener('click', () => {
     if (flag == 0) {
-      partTwo.style.transform = "translateX(0)"
+      partTwo.style.transform = "translateX(0)";
+      partTwo.style.display = "flex";
       flag = 1
     } else {
-      partTwo.style.transform = "translateX(100%)"
+      partTwo.style.transform = "translateX(100%)";
+      partTwo.style.display = "none";
       flag = 0
     }
 })
